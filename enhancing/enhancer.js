eda1b0628fd211314a50a47a9711d48ef1cdc40f
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-03-26 14:06:28 -0600

    mvp met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,54 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  let { enhancement } = item;
+
+  if (enhancement === 20) {
+    return {
+      ...item
+    };
+  } else {
+    enhancement = enhancement + 1;
+    return {
+      ...item,
+      enhancement
+    };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return {
+      ...item,
+      durability: item.durability - 5
+    };
+  } else if (item.enhancement >= 15 && item.enhancement > 16) {
+    return {
+      ...item,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+  } else if (item.enhancement === 16) {
+    return {
+      ...item,
+      durability: item.durability - 10
+    };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
-  return { ...item };
+  return {
+    ...item
+  };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,74 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair, fail, succeed } = require("./enhancer.js");
+const item1 = {
+  name: "BSinator",
+  durability: 100,
+  enhancement: 20
+};
+
+const item2 = {
+  name: "BFG 9000",
+  durability: 30,
+  enhancement: 16
+};
+const item3 = {
+  name: "lightsaber",
+  durability: 30,
+  enhancement: 12
+};
+
+describe("Test Suite", () => {
+  describe("Repair", () => {
+    it("durability should be 100", () => {
+      expect(repair(item3)).toStrictEqual({
+        ...item3,
+        durability: 100
+      });
+    });
+
+    it("nothing else change", () => {
+      expect(repair(item3)).toStrictEqual({
+        ...item3,
+        durability: 100
+      });
+    });
+  });
+
+  describe("Succeed", () => {
+    it("enhancement+1", () => {
+      expect(succeed(item3)).toStrictEqual({
+        ...item3,
+        enhancement: item3.enhancement + 1
+      });
+    });
+    it("not altered if 20", () => {
+      expect(succeed(item1)).toStrictEqual({
+        ...item1,
+        enhancement: 20
+      });
+    });
+  });
+
+  describe("Fail", () => {
+    it("decrease durability by 5if<15", () => {
+      expect(fail(item3)).toStrictEqual({
+        ...item3,
+        durability: item3.durability - 5
+      });
+    });
+
+    it("-5durability + -1enhancement if >16", () => {
+      expect(fail(item1)).toStrictEqual({
+        ...item1,
+        durability: item1.durability - 10,
+        enhancement: item1.enhancement - 1
+      });
+    });
+
+    it("if enhancement=16,-10 durability ", () => {
+      expect(fail(item2)).toStrictEqual({
+        ...item2,
+        durability: item2.durability - 10
+      });
+    });
+  });
+});
